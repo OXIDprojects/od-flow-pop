27a9fa86b5cacc894130f85f4306488d3c0ca230
rafig25 <info@oxid-design.com>
2016-10-20 15:56:19 +0200

    Create od_flow_pop.sql

---
+++ modules/OXID-Design/OD-FlowPop/sql/od_flow_pop.sql
@@ -0,0 +1,2 @@
+INSERT INTO `oxcontents` (`OXID`, `OXLOADID`, `OXSHOPID`, `OXSNIPPET`, `OXTYPE`, `OXACTIVE`, `OXACTIVE_1`, `OXPOSITION`, `OXTITLE`, `OXCONTENT`, `OXTITLE_1`, `OXCONTENT_1`, `OXACTIVE_2`, `OXTITLE_2`, `OXCONTENT_2`, `OXACTIVE_3`, `OXTITLE_3`, `OXCONTENT_3`, `OXCATID`, `OXFOLDER`, `OXTERMVERSION`, `OXTIMESTAMP`) VALUES
+('odflowpop', 'odflowpop', 'oxbaseshop', 1, 0, 1, 1, '', 'Herzlich Willkommen', 'Das ist ein beispiel text.', 'Welcome', 'This is a sample text.', 1, '', '', 1, '', '', 'odflowpop', 'CMSFOLDER_USERINFO', '', '2016-10-18 22:38:26');

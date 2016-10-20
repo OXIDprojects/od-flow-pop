db8e8293da1c72be77a072124799f817518cb561
rafig25 <info@oxid-design.com>
2016-10-20 17:28:25 +0200

    Create od_flow_pop_lang.php

---
+++ modules/OXID-Design/OD-FlowPop/views/admin/en/od_flow_pop_lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+//(C) OXID-Design 2015
+//
+// The source contained in this file is the property of the OXID-Design
+// (Leipziger Allee 36, 76139 Karlsruhe, Germany, www.oxid-design.com).
+//
+// GNU GENERAL PUBLIC LICENSE
+//
+//This program is free software;
+//you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+//either version 3 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+*/
+
+$sLangName  = "English";
+$aLang = array(
+    'charset'                          => 'ISO-8859-15',
+    'SHOP_MODULE_GROUP_odFlowPop'      => 'Flow-Popup settings',
+    'SHOP_MODULE_odHomePop'            => 'Show popup only on the start page',
+    'SHOP_MODULE_odOpenPop'            => 'Open popup only after specified milliseconds',
+    'SHOP_MODULE_odClosePop'           => 'Close popup after specified milliseconds'
+);
